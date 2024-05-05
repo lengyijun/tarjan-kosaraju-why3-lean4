946088dab5d71c0bbf3fd042cfa43a39d124ce3e
lengyijun <sjtu5140809011@gmail.com>
2024-05-05 10:14:10 +0800

    wip

--- kosaraju-lean4/Tarjan/Dfs.lean
+++ kosaraju-lean4/Tarjan/Dfs.lean
@@ -9,6 +9,8 @@ open Finmap
 open Finset
 open Finset List
 
+set_option maxHeartbeats 0
+
 structure Flair [DirectedGraph V Graph]
                 [BEq V] [LawfulBEq V] [DecidableEq V]
                 (graph : Graph) (x : V) (e : Env V Graph graph)
@@ -43,25 +45,69 @@ def dfs1 [DirectedGraph V Graph]
          (graph : Graph) (x : V) (e : Env V Graph graph)
          (a₁ : x ∈ DirectedGraph.vertices graph)
          (a₂ : access_to graph e.gray x)
-         (a₃ : ¬ x ∈ e.black ∪ e.gray)
+         (a₃ : ¬ x ∈ e.gray ∪ e.black)
          (a₄ : wf_env e)
-         : Int ×  Env V Graph graph :=
+         : Flair graph x e :=
 let n0 := e.sn
-let (n1, e1) := dfs graph (DirectedGraph.succ graph x) (add_stack_incr x e) sorry sorry sorry
+have h := by intros y hy z hz
+             rw [DirectedGraph.edge_succ] at hy
+             simp [add_stack_incr] at hz
+             cases hz with
+             | inl _ => subst z; tauto
+             | inr hz => specialize a₂ z hz
+                         apply reachable_trans graph z x y a₂
+                         tauto
+have h₁ : wf_env (add_stack_incr x e) := by
+  simp [add_stack_incr]
+  obtain ⟨_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _⟩ := a₄
+  repeat any_goals apply And.intro
+  any_goals simp
+  any_goals intros
+  repeat any_goals split
+  any_goals subst x
+  any_goals tauto
+  any_goals simp_all
+  any_goals tauto
+  any_goals intros
+  all_goals sorry -- 11 goals
+
+have termination_proof : ((add_stack_incr x e).gray ∪ (add_stack_incr x e).black).card > (e.gray ∪ e.black).card := by
+  simp [add_stack_incr]
+  have h := card_insert_of_not_mem a₃
+  rw [h]
+  omega
+
+let ⟨n1, e1, p₁, p₂, p₃, p₄, p₅, p₆⟩ := dfs graph (DirectedGraph.succ graph x) (add_stack_incr x e) (by apply succ_valid; assumption) h h₁
 let (s2, s3) := split x e1.stack
 let infty : Int := (DirectedGraph.vertices graph: List V).length
-if n1 < n0 then
-  (n1, add_black x e1)
+if dite : n1 < n0 then
+  {
+    n := n1
+    e' := add_black x e1
+    p₁ := by sorry
+    p₂ := by sorry
+    p₃ := by sorry
+    p₄ := by sorry
+    p₅ := by sorry
+  }
 else
-  ((DirectedGraph.vertices graph: List V).length, {
-    black := insert x e1.black
-    gray := e.gray
-    stack := s3
-    sccs := (toFinset s2) :: e1.sccs
-    sn := e1.sn
-    num := fun (y: V) => if s2.contains y then infty else e1.num y
-  })
-termination_by (Finset.card (((toFinset (DirectedGraph.vertices graph : List V)) \ (e.black ∪ e.gray)) : Finset V), 0)
+  {
+    n := (DirectedGraph.vertices graph: List V).length
+    e' := {
+            black := insert x e1.black
+            gray := e.gray
+            stack := s3
+            sccs := (toFinset s2) :: e1.sccs
+            sn := e1.sn
+            num := fun (y: V) => if s2.contains y then infty else e1.num y
+          }
+    p₁ := by sorry
+    p₂ := by sorry
+    p₃ := by sorry
+    p₄ := by sorry
+    p₅ := by sorry
+  }
+termination_by (Finset.card (((toFinset (DirectedGraph.vertices graph : List V)) \ (e.gray ∪ e.black)) : Finset V), 0)
 
 
 def dfs [DirectedGraph V Graph]
@@ -70,15 +116,79 @@ def dfs [DirectedGraph V Graph]
         (a₁ : roots ⊆ DirectedGraph.vertices graph)
         (a₂ : ∀ x, x ∈ roots -> access_to graph e.gray x)
         (a₃ : wf_env e)
-        : Int × (Env V Graph graph) := match roots with
-| [] => ((DirectedGraph.vertices graph: List V).length, e)
+        : Shuttle graph roots e := match roots with
+| [] => {
+           n := (DirectedGraph.vertices graph: List V).length
+           e' := e
+           p₁ := by repeat any_goals apply And.intro
+                    any_goals tauto
+                    any_goals simp_all
+           p₂ := a₃
+           p₃ := by intros; tauto
+           p₄ := by intros; tauto
+           p₅ := by intros; tauto
+           p₆ := by intros _ h
+                    obtain ⟨⟨s2, h₁, ⟨x, h₃, _⟩⟩, _⟩ := h
+                    symm at h₁
+                    rw [List.append_left_eq_self] at h₁
+                    subst s2
+                    tauto
+        }
 | x :: roots =>
-  let (n1, e1) := if (e.num x) == -1 then
-                    dfs1 graph x e sorry sorry sorry sorry
-                  else
-                    (e.num x, e)
-  let (n2, e2) := dfs graph roots e1 sorry sorry sorry
-  (min n1 n2, e2)
-termination_by (Finset.card (((toFinset (DirectedGraph.vertices graph : List V)) \ (e.black ∪ e.gray)) : Finset V), roots.length)
+  if dite: (e.num x) == -1 then
+    have h := by obtain ⟨_, _, _, h, _⟩ := a₃
+                 rw [<- h]
+                 simp at dite
+                 exact dite
+    let ⟨n1, e1, p₁, p₂, p₃, p₄, p₅⟩ := dfs1 graph x e (by tauto) (by apply a₂; tauto) h a₃
+    have h := by obtain ⟨h, _, _⟩ := p₁
+                 rw [<- h]
+                 intros
+                 apply a₂
+                 tauto
+    let ⟨n2, e2, p₆, p₇, p₈, p₉, pₐ, p⟩ := dfs graph roots e1 (by tauto) h p₂
+    {
+      n :=  min n1 n2
+      e' := e2
+      p₁ := subenv_trans p₁ p₆
+      p₂ := p₇
+      p₃ := by sorry
+      p₄ := by sorry
+      p₅ := by sorry
+      p₆ := by sorry
+    }
+  else
+    let n1 := e.num x
+    have h := by intros
+                 apply a₂
+                 tauto
+    let ⟨n2, e2, p₁, p₂, p₃, p₄, p₅, p₆⟩ := dfs graph roots e (by tauto) h a₃
+    {
+      n :=  min n1 n2
+      e' := e2
+      p₁ := p₁
+      p₂ := p₂
+      p₃ := by intros _ h
+               cases h
+               . have h : x ∈ e.gray ∪ e.black \/ ¬ x ∈ e.gray ∪ e.black := by tauto
+                 cases h with
+                 | inl h => obtain ⟨_, _, _⟩ := p₁
+                            simp_all
+                            cases h <;> tauto
+                 | inr h => obtain ⟨_, _, _, a₃, _⟩ := a₃
+                            rw [<- a₃] at h
+                            simp_all
+               . apply p₃; assumption
+      p₄ := by intros y h
+               cases h <;> rename_i h
+               . sorry
+               . specialize p₄ y h
+                 omega
+      p₅ := by sorry
+      p₆ := by intros y h
+               specialize p₆ y h
+               omega
+    }
+termination_by (Finset.card (((toFinset (DirectedGraph.vertices graph : List V)) \ (e.gray ∪ e.black)) : Finset V), roots.length)
 
 end
--- kosaraju-lean4/Tarjan/Env.lean
+++ kosaraju-lean4/Tarjan/Env.lean
@@ -20,28 +20,48 @@ where
   num:  V -> Int
 
 def subenv [DirectedGraph V Graph] [BEq V] [LawfulBEq V] [DecidableEq V] (e e': Env V Graph graph) : Prop :=
-e.gray == e'.gray /\
+e.gray = e'.gray /\
 e.black ⊆ e'.black /\
 e.sccs ⊆ e'.sccs /\
 (∀ x, x ∈ e.stack -> e.num x = e'.num x) /\
 (∃ s, e'.stack = s ++ e.stack /\ ∀ x, x ∈ s → x ∈ e'.black)
 
+theorem subenv_trans [DirectedGraph V Graph] [BEq V] [LawfulBEq V] [DecidableEq V]
+                    {e1 e2 e3: Env V Graph graph}
+                    (h12 : subenv e1 e2)
+                    (h23 : subenv e2 e3)
+                    : subenv e1 e3 := by
+obtain ⟨ _,  _,  _, h₄, h₅, h₆, h₇⟩ := h12
+obtain ⟨k₁, k₂, k₃, k₄, k₅, k₆, k₇⟩ := h23
+repeat any_goals apply And.intro
+any_goals tauto
+any_goals simp_all
+use (k₅ ++ h₅)
+simp_all
+intros _ h
+cases h
+. apply k₇; assumption
+. apply k₂
+  apply h₇
+  assumption
+
+
 def wf_env [DirectedGraph V Graph] [BEq V] [LawfulBEq V] [DecidableEq V] (e : Env V Graph graph) : Prop :=
 let infty : Int := (DirectedGraph.vertices graph: List V).length
 let sccs_union : Finset V := List.foldl (· ∪ ·) ∅ e.sccs
-(∀ x, (-1 ≤ e.num x && e.num x < (e.sn: Int)) || e.num x == infty) /\
 e.sn = Finset.card (e.gray ∪ e.black) /\
+(∀ x, (-1 ≤ e.num x /\ e.num x < (e.sn: Int)) \/ e.num x == infty) /\
 (∀ x, e.num x = infty <-> x ∈ sccs_union) /\
 (∀ x, e.num x = (-1)  <-> ¬ x ∈ (e.gray ∪ e.black)) /\
-(∀ x y, x ∈ e.stack → y ∈ e.stack → e.num x <= e.num y ↔ precedes y x e.stack) /\
 (∀ x, x ∈ e.gray  -> x ∈ DirectedGraph.vertices graph) /\
 (∀ x, x ∈ e.black -> x ∈ DirectedGraph.vertices graph) /\
-e.black ∩ e.gray == ∅ /\
+e.black ∩ e.gray = ∅ /\
 sccs_union ⊆ e.black /\
 toFinset e.stack = (e.gray ∪ (e.black \ sccs_union)) /\
 no_black_to_white graph e.black e.gray /\
 simplelist e.stack /\
-(∀ x y, x ∈ e.stack -> y ∈ e.stack -> e.num x <= e.num y -> reachable graph x y) /\
+(∀ x y, x ∈ e.stack → y ∈ e.stack → e.num x <= e.num y ↔ precedes y x e.stack) /\
+(∀ x y, x ∈ e.stack → y ∈ e.stack → e.num x <= e.num y → reachable graph x y) /\
 (∀ y, y ∈ e.stack -> ∃ x, x ∈ e.gray /\ e.num x <= e.num y /\ reachable graph y x) /\
 (∀ cc, cc ∈ e.sccs <-> cc ⊆ e.black /\ is_scc graph cc) /\
 (∀ cc₁ cc₂, cc₁ ∈ e.sccs -> cc₂ ∈ e.sccs -> cc₁ = cc₂ \/ cc₁ ∩ cc₂ = ∅)
@@ -70,3 +90,44 @@ def num_of_reachable [DirectedGraph V Graph]
                      [BEq V] [LawfulBEq V] [DecidableEq V]
                      (n: Int) (x: V) (e: Env V Graph graph) : Prop :=
 ∃ y, y ∈ e.stack /\ n = e.num y /\ reachable graph x y
+
+private theorem num_lmem_inner [DirectedGraph V Graph]
+                               [BEq V] [LawfulBEq V] [DecidableEq V]
+                               {e : Env V Graph graph}
+                               (h : wf_env e)
+                               (x : V) :
+let infty : Int := (DirectedGraph.vertices graph: List V).length
+(¬ e.num x = -1) /\ (¬ e.num x = infty) <-> x ∈ toFinset e.stack := by
+obtain ⟨h₆, h₅, h₃, h₄, h₇, h₈, h₉, h₂, h, _⟩ := h
+rw [h]
+simp
+have h₅ := not_congr (h₃ x)
+have h₆ := not_congr (h₄ x)
+rw [h₅, h₆]
+simp
+constructor
+. tauto
+. intros h
+  cases h with
+  | inl h =>
+    constructor
+    . tauto
+    . intros h₁
+      specialize h₂ h₁
+      simp_all
+      have h : x ∈ e.black ∩ e.gray := by simp [Inter.inter]; tauto
+      rw [h₉] at h
+      tauto
+  | inr _ => tauto
+
+theorem num_lmem [DirectedGraph V Graph]
+                 [BEq V] [LawfulBEq V] [DecidableEq V]
+                 {e : Env V Graph graph}
+                 (h : wf_env e)
+                 (x : V) :
+let infty : Int := (DirectedGraph.vertices graph: List V).length
+(¬ e.num x = -1) /\ (¬ e.num x = infty) <-> x ∈ e.stack := by
+intros
+have h := num_lmem_inner h x
+simp at h
+rw [h]
