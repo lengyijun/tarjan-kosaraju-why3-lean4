274a394f985352a51f9af4c3c9a24bee84220a95
lengyijun <sjtu5140809011@gmail.com>
2024-05-08 09:53:04 +0800

    wip: no wf_env

--- kosaraju-lean4/Tarjan/Env.lean
+++ kosaraju-lean4/Tarjan/Env.lean
@@ -9,6 +9,7 @@ open Finset
 open Finset List
 
 structure Env (V : Type*) {Graph : Type*}
+              [BEq V] [LawfulBEq V] [DecidableEq V]
               [DirectedGraph V Graph]
               (graph : Graph)
 where
@@ -17,9 +18,22 @@ where
   stack: List V
   sccs: List (Finset V)
   num:  V -> Int
+  num_clamp : ∀ x, (-1 ≤ num x /\ num x < gray.card + black.card) \/ num x = (DirectedGraph.vertices graph: List V).length
+  num_1 : ∀ x, num x = (-1)  <-> ¬ x ∈ (gray ∪ black)
+  num_infty : ∀ x, num x = (DirectedGraph.vertices graph: List V).length ↔ ∃ cc, x ∈ cc /\ cc ∈ sccs
+  valid_gray  : ∀ x ∈ gray,  x ∈ DirectedGraph.vertices graph
+  valid_black : ∀ x ∈ black, x ∈ DirectedGraph.vertices graph
+  disjoint_gb : Disjoint gray black
+  color₁ : no_black_to_white graph black gray
+  -- color₆ : sccs_union ⊆ black
+  stack_finset : toFinset stack = (gray ∪ (black \ sccs.foldl (· ∪ ·) ∅))
+  simplelist_stack : simplelist stack
+  wf_stack₁ : ∀ x ∈ stack, ∀ y ∈ stack, num x ≤ num y ↔ precedes y x stack
+  wf_stack₂ : ∀ x ∈ stack, ∀ y ∈ stack, num x ≤ num y → reachable graph x y
+  wf_stack₃ : ∀ y ∈ stack, ∃ x, x ∈ gray /\ num x ≤ num y /\ reachable graph y x
+  wf_sccs₁ : ∀ cc, cc ∈ sccs ↔ cc ⊆ black /\ is_scc graph cc
+  wf_sccs₂ : ∀ cc₁ ∈ sccs, ∀ cc₂ ∈ sccs, cc₁ = cc₂ \/ cc₁ ∩ cc₂ = ∅
 
-def sn [DirectedGraph V Graph] {graph : Graph} (e: Env V graph) : Int :=
-Finset.card e.gray + Finset.card e.black
 
 def subenv [DirectedGraph V Graph] [BEq V] [LawfulBEq V] [DecidableEq V] {graph : Graph} (e e': Env V graph) : Prop :=
 e.gray = e'.gray /\
@@ -50,25 +64,7 @@ cases h
   assumption
 
 
-def wf_env [DirectedGraph V Graph] [BEq V] [LawfulBEq V] [DecidableEq V] {graph : Graph} (e : Env V graph) : Prop :=
-let infty : Int := (DirectedGraph.vertices graph: List V).length
-let sccs_union : Finset V := List.foldl (· ∪ ·) ∅ e.sccs
-(∀ x, (-1 ≤ e.num x /\ e.num x < (sn e)) \/ e.num x == infty) /\
-(∀ x, e.num x = infty <-> x ∈ sccs_union) /\
-(∀ x, e.num x = (-1)  <-> ¬ x ∈ (e.gray ∪ e.black)) /\
-(∀ x, x ∈ e.gray  -> x ∈ DirectedGraph.vertices graph) /\
-(∀ x, x ∈ e.black -> x ∈ DirectedGraph.vertices graph) /\
-Disjoint e.gray e.black  /\
-sccs_union ⊆ e.black /\
-toFinset e.stack = (e.gray ∪ (e.black \ sccs_union)) /\
-no_black_to_white graph e.black e.gray /\
-simplelist e.stack /\
-(∀ x y, x ∈ e.stack → y ∈ e.stack → e.num x <= e.num y ↔ precedes y x e.stack) /\
-(∀ x y, x ∈ e.stack → y ∈ e.stack → e.num x <= e.num y → reachable graph x y) /\
-(∀ y, y ∈ e.stack -> ∃ x, x ∈ e.gray /\ e.num x <= e.num y /\ reachable graph y x) /\
-(∀ cc, cc ∈ e.sccs <-> cc ⊆ e.black /\ is_scc graph cc) /\
-(∀ cc₁ cc₂, cc₁ ∈ e.sccs -> cc₂ ∈ e.sccs -> cc₁ = cc₂ \/ cc₁ ∩ cc₂ = ∅)
-
+/-
 def add_black [DirectedGraph V Graph]
               [BEq V] [LawfulBEq V] [DecidableEq V]
               {graph : Graph}
@@ -90,6 +86,7 @@ def add_stack_incr [DirectedGraph V Graph]
         --  sn    := e.sn + 1
          num   := fun y => if y = x then sn e else e.num y
 }
+-/
 
 def num_of_reachable [DirectedGraph V Graph]
                      [BEq V] [LawfulBEq V] [DecidableEq V]
@@ -112,20 +109,82 @@ use x
 obtain ⟨_, _, _, h, ⟨s, h₂⟩⟩ := h
 simp_all
 
+theorem stack_or_scc [DirectedGraph V Graph]
+                     [BEq V] [LawfulBEq V] [DecidableEq V]
+                     {graph : Graph}
+                     {e : Env V graph}
+                     (x : V)
+                     (h₁ : x ∈ e.gray \/ x ∈ e.black) :
+x ∈ e.stack \/ (∃ cc, x ∈ cc /\ cc ∈ e.sccs) := by
+have h₂ : x ∈ List.foldl (fun x x_1 => x ∪ x_1) ∅ e.sccs \/ ¬ x ∈ List.foldl (fun x x_1 => x ∪ x_1) ∅ e.sccs  := by tauto
+cases h₂
+. right
+  rename_i h
+  rw [union_helper] at h
+  assumption
+. left
+  have h : x ∈ toFinset e.stack := by
+    rw [e.stack_finset]
+    simp
+    tauto
+  simp at h
+  assumption
+
+theorem jiqian [DirectedGraph V Graph]
+               [BEq V] [LawfulBEq V] [DecidableEq V]
+               {graph : Graph}
+               {e : Env V graph} :
+Finset.toList (e.gray ∪ e.black) ⊆ (DirectedGraph.vertices graph: List V) := by
+intro x h₁
+simp_all
+cases h₁
+. apply e.valid_gray; assumption
+. apply e.valid_black; assumption
+
+theorem navel [DirectedGraph V Graph]
+              [BEq V] [LawfulBEq V] [DecidableEq V]
+              {graph : Graph}
+              (e : Env V graph) :
+(Finset.toList (e.gray ∪ e.black)).length ≤ (DirectedGraph.vertices graph: List V).length := by
+apply List.Subperm.length_le
+apply List.subperm_of_subset
+. apply Finset.nodup_toList
+. apply jiqian
+
+theorem sn_bound [DirectedGraph V Graph]
+                 [BEq V] [LawfulBEq V] [DecidableEq V]
+                 {graph : Graph}
+                 (e : Env V graph) :
+e.gray.card + e.black.card ≤ (DirectedGraph.vertices graph: List V).length := by
+have h₁ := navel e
+simp at h₁
+rw [Finset.card_union_of_disjoint e.disjoint_gb] at h₁
+omega
+
+theorem num_bound [DirectedGraph V Graph]
+                    [BEq V] [LawfulBEq V] [DecidableEq V]
+                    {graph : Graph}
+                    (e : Env V graph)
+                    {x : V} :
+e.num x ≤ (DirectedGraph.vertices graph: List V).length := by
+have := sn_bound e
+have h := e.num_clamp
+cases (h x) with
+| inl h => omega
+| inr h => simp at h
+           omega
+
 private theorem num_lmem_inner [DirectedGraph V Graph]
                                [BEq V] [LawfulBEq V] [DecidableEq V]
                                {graph : Graph}
                                {e : Env V graph}
-                               (h : wf_env e)
                                (x : V) :
-let infty : Int := (DirectedGraph.vertices graph: List V).length
-(¬ e.num x = -1) /\ (¬ e.num x = infty) <-> x ∈ toFinset e.stack := by
-obtain ⟨h₅, h₃, h₄, h₇, h₈, h₉, h₂, h, _⟩ := h
-rw [h]
+(¬ e.num x = -1) /\ (¬ e.num x = (DirectedGraph.vertices graph: List V).length) ↔ x ∈ toFinset e.stack := by
+rw [e.stack_finset]
 simp
-have h₅ := not_congr (h₃ x)
-have h₆ := not_congr (h₄ x)
-rw [h₅, h₆]
+have h₅ := not_congr (e.num_1 x)
+have h₆ := not_congr (e.num_infty x)
+rw [h₅, h₆, <- union_helper]
 simp
 constructor
 . tauto
@@ -135,97 +194,21 @@ constructor
     constructor
     . tauto
     . intros h₁
-      specialize h₂ h₁
-      simp_all
-      have h₂ : {x} ≤ e.gray := by simp; assumption
-      have h : {x} ≤ e.black := by simp; assumption
-      specialize h₉ h₂ h
-      simp at h₉
+      rw [union_helper] at h₁
+      obtain ⟨cc, h₁, h₃⟩ := h₁
+      have hg : {x} ≤ e.gray := by simp; assumption
+      have hb : {x} ≤ e.black := by simp
+                                    rw [e.wf_sccs₁] at h₃
+                                    tauto
+      have h := e.disjoint_gb hg hb
+      simp at h
   | inr _ => tauto
 
 theorem num_lmem [DirectedGraph V Graph]
                  [BEq V] [LawfulBEq V] [DecidableEq V]
                  {graph : Graph}
-                 {e : Env V graph}
-                 (h : wf_env e)
+                 (e : Env V graph)
                  (x : V) :
 (¬ e.num x = -1) /\ (¬ e.num x = (DirectedGraph.vertices graph: List V).length) ↔ x ∈ e.stack := by
-intros
-have h := num_lmem_inner h x
-simp at h
-rw [h]
-
-theorem stack_or_scc [DirectedGraph V Graph]
-                     [BEq V] [LawfulBEq V] [DecidableEq V]
-                     {graph : Graph}
-                     {e : Env V graph}
-                     (h : wf_env e)
-                     (x : V)
-                     (h₁ : x ∈ e.gray \/ x ∈ e.black) :
-x ∈ e.stack \/ (∃ cc, x ∈ cc /\ cc ∈ e.sccs) := by
-obtain ⟨_, _, _, _, _, _, _, h₃, _⟩ := h
-have h₂ : x ∈ List.foldl (fun x x_1 => x ∪ x_1) ∅ e.sccs \/ ¬ x ∈ List.foldl (fun x x_1 => x ∪ x_1) ∅ e.sccs  := by tauto
-cases h₂
-. right
-  rename_i h
-  rw [union_helper] at h
-  assumption
-. left
-  have h : x ∈ toFinset e.stack := by
-    rw [h₃]
-    simp
-    tauto
-  simp at h
-  assumption
-
-theorem jiqian [DirectedGraph V Graph]
-               [BEq V] [LawfulBEq V] [DecidableEq V]
-               {graph : Graph}
-               {e : Env V graph}
-               (h : wf_env e) :
-Finset.toList (e.gray ∪ e.black) ⊆ (DirectedGraph.vertices graph: List V) := by
-intro x h₁
-simp_all
-obtain ⟨_, _, _, h₂, h₃, _⟩ := h
-cases h₁ <;> tauto
-
-theorem navel [DirectedGraph V Graph]
-              [BEq V] [LawfulBEq V] [DecidableEq V]
-              {graph : Graph}
-              {e : Env V graph}
-              (h : wf_env e) :
-(Finset.toList (e.gray ∪ e.black)).length ≤ (DirectedGraph.vertices graph: List V).length := by
-apply List.Subperm.length_le
-apply List.subperm_of_subset
-. apply Finset.nodup_toList
-. apply jiqian
-  assumption
-
-
-theorem sn_bound [DirectedGraph V Graph]
-                 [BEq V] [LawfulBEq V] [DecidableEq V]
-                 {graph : Graph}
-                 {e : Env V graph}
-                 (h : wf_env e) :
-sn e ≤ (DirectedGraph.vertices graph: List V).length := by
-simp [sn]
-have h₁ := navel h
-simp at h₁
-obtain ⟨h, _, _, _, _, h₂, _⟩ := h
-rw [Finset.card_union_of_disjoint h₂] at h₁
-omega
-
-theorem num_bound [DirectedGraph V Graph]
-                    [BEq V] [LawfulBEq V] [DecidableEq V]
-                    {graph : Graph}
-                    {e : Env V graph}
-                    (h : wf_env e)
-                    {x : V} :
-e.num x ≤ (DirectedGraph.vertices graph: List V).length := by
-have := sn_bound h
-obtain ⟨h, _, _, _⟩ := h
-specialize h x
-cases h with
-| inl h => omega
-| inr h => simp at h
-           omega
+rw [num_lmem_inner]
+simp
