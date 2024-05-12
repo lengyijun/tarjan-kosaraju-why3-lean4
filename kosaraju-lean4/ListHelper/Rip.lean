93edab34cd45488b0558196a1ad26d20c2d6910a
lengyijun <sjtu5140809011@gmail.com>
2024-05-12 18:31:36 +0800

    split_once

--- kosaraju-lean4/ListHelper/Rip.lean
+++ kosaraju-lean4/ListHelper/Rip.lean
@@ -3,34 +3,26 @@ import Mathlib.Tactic.Tauto
 
 structure Rapport (x : V) (s : List V) where
   s1 : List V
-  s2 : List V
-  combine : s1 ++ s2 = s
-  last : x ∈ s -> is_last x s1
+  last : x ∈ s -> ∃ s2, s1 ++ x :: s2 = s
 
-def rip [DecidableEq V] (x : V) (s: List V) : Rapport x s := match s with
+def split_once [DecidableEq V] (x : V) (s: List V) : Rapport x s := match s with
 | [] => {
           s1 := []
-          s2 := []
-          combine := by tauto
           last := by tauto
         }
 | y :: s => if dite : x = y then
               {
-                s1 := x :: []
-                s2 := s
-                combine := by subst y; tauto
+                s1 := []
                 last := by tauto
               }
             else
-              let ⟨s1, s2, combine, last⟩ := rip x s
+              let ⟨s1, last⟩ := split_once x s
               {
                 s1 := y :: s1
-                s2 := s2
-                combine := by subst s; tauto
                 last := by intros h
                            cases h
                            . tauto
                            . obtain ⟨s3, h⟩ := last (by tauto)
-                             exists (y :: s3)
-                             tauto
+                             subst s
+                             exists s3
               }
