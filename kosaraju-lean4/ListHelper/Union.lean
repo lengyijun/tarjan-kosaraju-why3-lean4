55f1d6634a59c57366139017208d26ec8e25b746
lengyijun <sjtu5140809011@gmail.com>
2024-05-14 10:20:07 +0800

    union

--- kosaraju-lean4/ListHelper/Union.lean
+++ kosaraju-lean4/ListHelper/Union.lean
@@ -7,30 +7,11 @@ theorem pertinent
 ∀ (init : Finset V),
 List.foldl (fun x x_1 => x ∪ x_1) init sccs = init ∪ List.foldl (fun x x_1 => x ∪ x_1) ∅ sccs
 := by
-induction sccs
-. simp
-. intros init
-  simp
-  rename_i scc sccs induction_step
-  rw [induction_step, induction_step scc]
-  simp
-
-theorem jiting
-[BEq V] [LawfulBEq V] [DecidableEq V]
-{x : V} {sccs : List (Finset V)} :
-∀ (init : Finset V),
-x ∈ List.foldl (fun x x_1 => x ∪ x_1) init sccs ↔ x ∈ init \/ x ∈ List.foldl (fun x x_1 => x ∪ x_1) ∅ sccs
-:= by
-induction sccs
-. tauto
-. intros init
-  simp
-  rename_i head tail induction_step
-  have h₁ := induction_step (init ∪ head)
-  have h₂ := induction_step head
-  rw [h₁, h₂]
-  simp
-  tauto
+induction sccs <;> simp
+rename_i scc sccs induction_step
+intros
+rw [induction_step, induction_step scc]
+simp
 
 theorem union_helper
 [BEq V] [LawfulBEq V] [DecidableEq V]
@@ -38,9 +19,11 @@ theorem union_helper
 x ∈ List.foldl (fun x x_1 => x ∪ x_1) ∅ sccs ↔ (∃ cc, x ∈ cc /\ cc ∈ sccs) := by
 induction sccs
 . tauto
-. simp_all
+. simp
+  rw [pertinent]
+  simp
   rename_i h
-  rw [jiting, h]
+  rw [h]
   constructor <;> intros h <;> cases h
   . tauto
   . rename_i h
