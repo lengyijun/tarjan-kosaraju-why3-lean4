d59ae7b08b384582c2f668d8191eed7fef606113
lengyijun <sjtu5140809011@gmail.com>
2025-01-17 17:46:37 +0800

    cleaner, but noncomputable

--- kosaraju-tarjan-lean4/Graph/AdjacencyTable.lean
+++ kosaraju-tarjan-lean4/Graph/AdjacencyTable.lean
@@ -1,28 +1,13 @@
 import Graph.DirectedGraph
 import Mathlib.Data.Matrix.Basic
 
-def shepherd (n : Nat) : List (Fin n) :=
-  match n with
-  | Nat.zero => List.nil
-  | Nat.succ x => List.cons ⟨x, by simp⟩ (shepherd x)
-
-theorem all_in_shepherd {n : Nat} (x : Fin n) : x ∈ shepherd n := by
-  induction n with obtain ⟨val, is_lt⟩ := x
-  | zero => contradiction
-  | succ n =>
-      have h₂ : val < n \/ val = n := by omega
-      simp [shepherd]
-      cases h₂ with
-      | inl h₂ => right; use ⟨val, h₂⟩; constructor <;> tauto
-      | inr h₂ => tauto
-
 -- t[i][j] = true : i -> j
 structure AdjacencyTable (n : Nat) where
   t : Matrix (Fin n) (Fin n) Bool
 
-def AdjacencyTable.succ {n : ℕ}(graph : AdjacencyTable n) (node : Fin n) : List (Fin n) := List.filter (fun j => graph.t node j) (shepherd n)
+noncomputable def AdjacencyTable.succ {n : ℕ} (graph : AdjacencyTable n) (node : Fin n) : List (Fin n) := List.filter (fun j => graph.t node j) ((Finset.univ : Finset (Fin n)).toList)
 
-def AdjacencyTable.vertices {n : ℕ}(_g: AdjacencyTable n) : List (Fin n) := shepherd n
+noncomputable def AdjacencyTable.vertices {n : ℕ} (_g: AdjacencyTable n) : List (Fin n) := (Finset.univ : Finset (Fin n)).toList
 
 -- graph  : a -> b
 -- result : b -> a
@@ -36,7 +21,7 @@ simp [AdjacencyTable.transpose]
 theorem same_vertices {n : ℕ}: ∀ (g : AdjacencyTable n), g.transpose.vertices = g.vertices := by
  simp [AdjacencyTable.transpose, AdjacencyTable.vertices]
 
-instance {n : ℕ}: DirectedGraph (Fin n) (AdjacencyTable n) where
+noncomputable instance {n : ℕ}: DirectedGraph (Fin n) (AdjacencyTable n) where
   vertices := AdjacencyTable.vertices
   edge g a b := g.t a b = True
   succ      := AdjacencyTable.succ
@@ -45,6 +30,6 @@ instance {n : ℕ}: DirectedGraph (Fin n) (AdjacencyTable n) where
   same_vertices := same_vertices
   reverse_edge :=  by simp [AdjacencyTable.transpose]
   edge_succ := by simp [AdjacencyTable.succ]
-                  intros
-                  apply all_in_shepherd
-  valid_edge := by intros; constructor <;> apply all_in_shepherd
+  valid_edge := by intros
+                   unfold AdjacencyTable.vertices
+                   simp
